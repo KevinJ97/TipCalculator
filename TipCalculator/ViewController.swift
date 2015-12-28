9c653fa123377e711cfb6542bf18ba89f6a45b87
Kevin J Nguyen <kevinjnguyen2@gmail.com>
2015-12-28 00:54:05 -0600

    Added empty functions to all the buttons

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -27,6 +27,52 @@ class ViewController: UIViewController {
     @IBAction func buttonOne(sender: UIButton) {
         labelCost.text = "$1.00"
     }
+    
+    @IBAction func buttonTwo(sender: UIButton) {
+        labelCost.text = "$2.00"
+    }
 
+    @IBAction func buttonThree(sender: UIButton) {
+    }
+    
+    @IBAction func buttonFour(sender: UIButton) {
+    }
+    
+    @IBAction func buttonFive(sender: UIButton) {
+    }
+    
+    @IBAction func buttonSix(sender: UIButton) {
+    }
+    
+    @IBAction func buttonSeven(sender: UIButton) {
+    }
+    
+    @IBAction func buttonEight(sender: UIButton) {
+    }
+    
+    @IBAction func buttonNie(sender: UIButton) {
+    }
+    
+    @IBAction func buttonDecimal(sender: UIButton) {
+    }
+    
+    @IBAction func buttonZero(sender: UIButton) {
+    }
+    
+    @IBAction func buttonClear(sender: UIButton) {
+    }
+    
+    @IBAction func button15P(sender: UIButton) {
+    }
+    
+    @IBAction func button18P(sender: UIButton) {
+    }
+    
+    @IBAction func button20P(sender: UIButton) {
+    }
+    
+    @IBAction func buttonOtherP(sender: UIButton) {
+    }
+    
 }
 
