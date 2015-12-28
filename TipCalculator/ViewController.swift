d3508af5bd76f4cf3f5e2594caeba0b86bbbd93d
Kevin J Nguyen <kevinjnguyen2@gmail.com>
2015-12-28 09:12:03 -0600

    Working Calculator
    Need to fix other butter UITextfield

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     var currentTotal : Float = 0.00
     
     
+    @IBOutlet weak var labelTitleTip: UILabel!
     @IBOutlet weak var labelCost: UILabel!
     @IBOutlet weak var labelTip: UILabel!
     @IBOutlet weak var labelTotal: UILabel!
@@ -32,40 +33,47 @@ class ViewController: UIViewController {
 
     @IBAction func buttonOne(sender: UIButton) {
         currentCost = currentCost * 10 + 0.01
-        let formatCost : String = String(format: "%0.2f", currentCost)
-        labelCost.text = "$\(formatCost)"
+        printAll()
     }
     
     @IBAction func buttonTwo(sender: UIButton) {
         currentCost = currentCost * 10 + 0.02
+        printAll()
     }
 
     @IBAction func buttonThree(sender: UIButton) {
         currentCost = currentCost * 10 + 0.03
+        printAll()
     }
     
     @IBAction func buttonFour(sender: UIButton) {
         currentCost = currentCost * 10 + 0.04
+        printAll()
     }
     
     @IBAction func buttonFive(sender: UIButton) {
         currentCost = currentCost * 10 + 0.05
+        printAll()
     }
     
     @IBAction func buttonSix(sender: UIButton) {
         currentCost = currentCost * 10 + 0.06
+        printAll()
     }
     
     @IBAction func buttonSeven(sender: UIButton) {
         currentCost = currentCost * 10 + 0.07
+        printAll()
     }
     
     @IBAction func buttonEight(sender: UIButton) {
         currentCost = currentCost * 10 + 0.08
+        printAll()
     }
     @IBAction func buttonNine(sender: UIButton) {
         
         currentCost = currentCost * 10 + 0.09
+        printAll()
     }
     
     @IBAction func buttonDecimal(sender: UIButton) {
@@ -74,6 +82,7 @@ class ViewController: UIViewController {
     @IBAction func buttonZero(sender: UIButton) {
         
         currentCost = currentCost * 10
+        printAll()
     }
     
     @IBAction func buttonClear(sender: UIButton) {
@@ -81,17 +90,37 @@ class ViewController: UIViewController {
     }
     
     @IBAction func button15P(sender: UIButton) {
+        percentageTip = 0.15
+        labelTitleTip.text = "Tip (15%)"
+        printAll()
     }
     
     @IBAction func button18P(sender: UIButton) {
+        percentageTip = 0.18
+        labelTitleTip.text = "Tip (18%)"
+        printAll()
     }
     
     @IBAction func button20P(sender: UIButton) {
+        percentageTip = 0.20
+        labelTitleTip.text = "Tip (20%)"
+        printAll()
     }
     
     @IBAction func buttonOtherP(sender: UIButton) {
     }
     
+    func printAll()
+    {
+        let formatCost : String = String(format: "%0.2f", currentCost)
+        currentTip = percentageTip * currentCost
+        let formatTip : String = String(format: "%0.2f", currentTip)
+        currentTotal = currentCost + currentTip
+        let formatTotal : String = String(format: "%0.2f", currentTotal)
+        labelCost.text = "$\(formatCost)"
+        labelTip.text = "$\(formatTip)"
+        labelTotal.text = "$\(formatTotal)"
+    }
     
     func clearAll()
     {
